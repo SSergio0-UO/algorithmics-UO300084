352fcbe1be8bd577166359d2bceaf56148ab4dfc
SSergio0-UO <uo300084@uniovi.es>
2025-05-05 09:40:03 +0200

    changes lab4 for performance

--- algstudent/s4/Session 4/graph_colouring.py
+++ algstudent/s4/Session 4/graph_colouring.py
@@ -5,17 +5,29 @@ from helper import draw_coloured_map, generate_graph_map
 def greedy(map):#for solving it correctly u first need to paint the nodes with the highest number of connections
     colours = ["red", "blue", "green", "yellow", 
 "orange", "purple", "cyan", "magenta", "lime"]
+    
+
     solution = create_sol_list(map.keys());
+
+
     count = 0
     number_of_colours = 0
     colours_used = {}
-    for i in range (8,-1,-1):#start in 8 and goes down to 0
-        for node in map.keys():
-            if(len(map[node]) == i):
-                for colour in colours:
-                    if(check_colour_connections(map[node],colour,solution)):
-                        solution[node] = colour
-                        break 
+
+    nodes_sorted = sorted(map.keys(), key = lambda x: len(map[x]), reverse = True)
+    for node in nodes_sorted:
+        for colour in colours:
+            if(check_colour_connections(map[node], colour, solution)):
+                solution[node] = colour
+                break
+
+    # for i in range (8,-1,-1):#start in 8 and goes down to 0                       (not efficient)
+    #     for node in map.keys():
+    #         if(len(map[node]) == i):
+    #             for colour in colours:
+    #                 if(check_colour_connections(map[node],colour,solution)):
+    #                     solution[node] = colour
+    #                     break 
     return solution
 
 def check_colour_connections(node, colour,solution):
